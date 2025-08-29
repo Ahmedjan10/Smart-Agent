9312dc25d98487adccf6c3af27d782277f8aad1a
Ahmed <ahmedabdulqayyum54@gmail.com>
2025-08-29 18:57:36 +0500

    change the Robot

--- src/Componenets/Robot.jsx
+++ src/Componenets/Robot.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 
 export default function Robot(props) {
-  const { scene, animations } = useGLTF("/public/Robot.gltf"); // <-- your GLB file
+  const { scene, animations } = useGLTF("/Robot.gltf"); // <-- your GLB file
   const { actions } = useAnimations(animations, scene);
 
   useEffect(() => {
