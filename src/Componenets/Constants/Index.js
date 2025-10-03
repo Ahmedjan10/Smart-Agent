7a67a548573aa60709aa47ff7eef567c5ba41499
Ahmed <ahmedabdulqayyum54@gmail.com>
2025-10-03 23:00:33 +0500

    removed public

--- src/Componenets/Constants/Index.js
+++ src/Componenets/Constants/Index.js
@@ -1,15 +1,15 @@
 const logoIconsList = [
   {
-    imgPath: "public/Logos/2e69901e4dffac0dca5b361b2e122e0d-Photoroom.png",
+    imgPath: "/Logos/2e69901e4dffac0dca5b361b2e122e0d-Photoroom.png",
   },
   {
-    imgPath: "public/Logos/3b37e1b1b40dff8cb5e98fe1fc324bd3-Photoroom.png",
+    imgPath: "/Logos/3b37e1b1b40dff8cb5e98fe1fc324bd3-Photoroom.png",
   },
   {
-    imgPath: "public/Logos/4178d5a0a14d32e3d5672090d840ac58-Photoroom.png",
+    imgPath: "/Logos/4178d5a0a14d32e3d5672090d840ac58-Photoroom.png",
   },
   {
-    imgPath: "public/Logos/6335e9e01347f76258e36be6c552de32-Photoroom.png",
+    imgPath: "/Logos/6335e9e01347f76258e36be6c552de32-Photoroom.png",
   },
 
 ];
